4a8a4097f21ba281c979c932b5d931b619daa3f2
Rich McNeary <rlm2public@gmail.com>
2016-09-06 13:38:05 -0400

    Use React to render a root level component.

--- src/app/index.js
+++ src/app/index.js
@@ -2,16 +2,32 @@
 
 
 const ipc = require("../electron/renderer/ipc");
+const React = require("react");
+const ReactContent = require("./view/reactContent.jsx");
+const ReactDOM = require("react-dom");
+const reducers = require("./reducer/reducers");
+const redux = require("redux");
+
+
+const startupDelayMs = 1000; // Set this to allow the Electron application to load so the debugger can attach.
 
 
 setTimeout(() => {
     ipc.log("index.js - started.", "iii");
 
-    ipc.readTextFile("C:\\Users\\rich\\temp\\ftp_client.log", chunk => {
-        if (!chunk) {
-            console.log("All chunks read");
-        } else {
-            console.log(chunk);
-        }
-    });
-}, 1000);
+    const props = {
+        content: React.createElement("h1", null, "Hello React world content!"),
+        store: redux.createStore(reducers)
+    };
+
+    const reactContent = React.createElement(ReactContent, props);
+    ReactDOM.render(reactContent, document.getElementById("react-content"));
+
+    // ipc.readTextFile("C:\\Users\\rich\\temp\\ftp_client.log", chunk => {
+    //     if (!chunk) {
+    //         console.log("All chunks read");
+    //     } else {
+    //         console.log(chunk);
+    //     }
+    // });
+}, startupDelayMs);
---
+++ src/app/view/reactContent.jsx
@@ -0,0 +1,25 @@
+"use strict";
+
+
+const IntlProvider = require("react-intl").IntlProvider;
+const React = require("react");
+const Provider = require("react-redux").Provider;
+
+
+const reactContent = props => {
+    return (
+        <IntlProvider locale={props.locale || "en-US"} messages={props.messages || {}}>
+            <Provider children={props.content} store={props.store} />
+        </IntlProvider>
+    );
+};
+
+reactContent.propTypes = {
+    content: React.PropTypes.element.isRequired, // The application content.
+    locale: React.PropTypes.string, // The locale of messages and numbers to display. Defaults to en-US.
+    messages: React.PropTypes.string, // The ICU messages for display. Defaults to an empty object.
+    store: React.PropTypes.object.isRequired // The redux store.
+};
+
+
+module.exports = reactContent;
