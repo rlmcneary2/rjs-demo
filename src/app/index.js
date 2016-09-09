5ad93f2a262b763f4bf31a267479711a5044b2a2
Rich McNeary <rlm2public@gmail.com>
2016-09-09 13:02:52 -0400

    Code cleanup and include spanish locale.

--- src/app/index.js
+++ src/app/index.js
@@ -4,39 +4,30 @@
 const addLocaleData = require("react-intl").addLocaleData;
 const ipc = require("../electron/renderer/ipc");
 const React = require("react");
-const ReactContent = require("./view/reactContent.jsx");
+const ReactContent = require("./view/ReactContent.jsx");
 const ReactDOM = require("react-dom");
 const reducers = require("./reducer/reducers");
 const redux = require("redux");
 const url = require("url");
 
 
-const _content = require("./view/example.jsx");
-const _enUsMessages = require("../locale/en-US.json");
-const _startupDelayMs = 1000; // Set this to allow the Electron application to load so the debugger can attach.
+const _query = url.parse(window.location.href, true).query;
+const _startupDelayMs = _query["appStartupDelay"] ? _query["appStartupDelay"] : 0;
 
 
 setTimeout(() => {
     ipc.log("index.js - started.", "iii");
 
-    const query = url.parse(window.location.href, true).query;
-
     // Loading language information early is a good idea. Use the query to
     // configure the application after updateLocale resolves.
-    updateLocale(query)
+    updateLocale(_query)
         .then(({locale, messages}) => {
 
-
-            // Add functions to configure the application here.
+            // TODO: optional. Add functions to configure the application here.
 
             const content = createContent();
 
             const props = createReactContentProps(content, locale, messages);
-
-
-            // Include the application's root React component in the props as
-            // the content property.
-
 
             renderReactContent(props);
 
@@ -53,12 +44,13 @@ setTimeout(() => {
 
 
 function renderReactContent(props) {
-    const reactContent = React.createElement(ReactContent, props);
-    ReactDOM.render(reactContent, document.getElementById("react-content"));
+    const content = React.createElement(ReactContent, props);
+    ReactDOM.render(content, document.getElementById("react-content"));
 }
 
 function createContent(props) {
-    return React.createElement(_content, props);
+    const content = require("./view/App.jsx"); // Change to the location of your application's root component.
+    return React.createElement(content, props);
 }
 
 function createReactContentProps(content, locale, messages) {
@@ -72,7 +64,7 @@ function createReactContentProps(content, locale, messages) {
 
 function updateLocale(query = url.parse(window.location.href, true).query) {
     return new Promise(resolve => {
-        const locale = query["locale"];
+        const locale = query["current-locale"];
 
 
         // Three things to do here:
@@ -87,18 +79,20 @@ function updateLocale(query = url.parse(window.location.href, true).query) {
         }
 
         const localeData = {
-            en: require("react-intl/locale-data/en")
-            // Require the locale-data modules your application requires (see: "Locale Data as Modules" https://github.com/yahoo/react-intl/issues/162)
+            en: require("react-intl/locale-data/en"),
+            es: require("react-intl/locale-data/es")
+            // Require the locale-data modules the application needs (see: "Locale Data as Modules" https://github.com/yahoo/react-intl/issues/162)
         };
 
         if (language) {
             addLocaleData(localeData[language]);
         }
 
-        let messages = {};
-        if (_enUsMessages) {
-            messages["en-US"] = _enUsMessages;
-        }
+        let messages = {
+            "en-US": require("../locale/en-US.json"),
+            "es-ES": require("../locale/es-ES.json")
+            // Require other localized strings the application needs.
+        };
 
         // Get an object that contains the localized strings. That could be done by requiring the JSON files, downloading them from a server, or reading them from a file using IPC.
         // Return the object (messages) from the JSON file that matches the requested locale.
