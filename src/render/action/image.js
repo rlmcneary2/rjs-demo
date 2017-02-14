f673f4e79ee88ee1d457d808f5ca25576730fa04
Rich McNeary <rlmcneary2@gmail.com>
2017-02-13 23:43:15 -0500

    Add missing file.

--- src/render/action/image.js
+++ src/render/action/image.js
@@ -21,7 +21,6 @@ module.exports = {
 
                 ipc.getFlickrImage()
                     .then(result => {
-                        debugger;
                         dispatch(self.getFlickrImageEnd(result));
                     });
             };
